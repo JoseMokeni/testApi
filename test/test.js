dcdd8874f7b4a49d2d9849a5c8b4765f96bc87b4
José Mokeni <jmmokeni@gmail.com>
2024-03-05 10:16:51 +0100

    Test updated

--- test/test.js
+++ test/test.js
@@ -7,20 +7,6 @@ describe("GET /", () => {
     request(app)
       .get("/")
       .expect("Content-Type", /json/)
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        if (res.body.message) {
-          if (res.body.message === "Hello world!") done();
-          else
-            done(new Error("Expect the message to be equal to 'Hello world!'"));
-        } else {
-          done(
-            new Error(
-              "Expect the response to be a json object with a message property equal to 'Hello world!'"
-            )
-          );
-        }
-      });
+      .expect(200, { message: "Hello world!" }, done);
   });
 });
